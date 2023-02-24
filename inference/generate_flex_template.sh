b1a1a0d3dd5e2fd07fd70c0ec9323fa12473e994
Stefano Bosisio <stbo@trustpilot.com>
2023-02-24 15:23:42 +0000

    add flex tmepl

---
+++ inference/generate_flex_template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+function create_flex_template() {
+  TEMPLATE_IMAGE="eu.gcr.io/napierpipeline"
+  TEMPLATE_PATH="gs://napierpipeline/templates/template.json"
+
+  echo "${TEMPLATE_IMAGE}"
+  echo "${TEMPLATE_PATH}"
+
+  gcloud dataflow flex-template build "${TEMPLATE_PATH}" \
+  --image "${TEMPLATE_IMAGE}" \
+  --sdk-language "${SDK_LANGUAGE}" \
+  --enable-streaming-engine
+}
+
+function push_docker_image_to_gcr(){
+  echo "Creating Docker image"
+  TEMPLATE_IMAGE="eu.gcr.io/napierpipeline"
+  docker build -t "${TEMPLATE_IMAGE}" -f "${DOCKER_FILE}" .
+  gcloud config set project NapierUniversity
+  gcloud auth configure-docker eu.gcr.io --quiet
+  gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://eu.gcr.io
+  docker push "${TEMPLATE_IMAGE}"
+}
+
+# staging
+push_docker_image_to_gcr
+create_flex_template 
